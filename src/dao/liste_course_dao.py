526f40782f5532c46b64ee606103d8f5854dc838
LIZE Benjamin <id2399@DOMENSAI.ECOLE>
2024-10-22 12:39:15 +0200

    Toussaint : Ajouter recette à liste de course

--- src/dao/liste_course_dao.py
+++ src/dao/liste_course_dao.py
@@ -5,6 +5,7 @@ from utils.log_decorator import log
 
 from dao.db_connection import DBConnection
 
+
 class ListeCourseDao(metaclass=Singleton):
     """Classe contenant les méthodes pour gérer la liste de course"""
 
@@ -38,3 +39,93 @@ class ListeCourseDao(metaclass=Singleton):
 
         return res > 0
 
+    @log
+    def trouver_par_id(self, utilisateur, ingredient) -> bool:
+        """Déterminer si un couplage (utilisateur, ingrédient)
+            donné existe dans la liste de courses
+
+        Parameters
+        ----------
+        utilisateur : Utilisateur
+        ingredient : Ingredient
+
+        Returns
+        -------
+         True si ce couplage existe déjà et False sinon
+        """
+        try:
+            with DBConnection().connection as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(
+                        "SELECT *                           "
+                        "  FROM liste_course                      "
+                        " WHERE id_utilisateur = %(id_utilisateur)s "
+                        " AND id_ingredient = %(id_ingredient)s;  "
+                        )
+
+                    res = cursor.fetchone()
+        except Exception as e:
+            logging.info(e)
+            raise
+
+        return res is not None
+
+    @log
+    def ajouter_recette_a_liste(self, utilisateur, recette) -> bool:
+        """Ajout de tous les ingrédients d'une recette à la liste de courses d'un utilisateur donné
+
+        Parameters
+        ----------
+        utilisateur : Utilisateur
+        recette : Recette
+
+        Returns
+        -------
+        created : bool
+            True si l'ajout est un succès, False sinon
+        """
+
+        try:
+            with DBConnection().connection as connection:
+                with connection.cursor() as cursor:
+                    # Récupération des ingrédients de la recette
+                    cursor.execute(
+                        """
+                        SELECT i.id_ingredient
+                        FROM ingredient i
+                        JOIN recette_ingredient ri ON i.id_ingredient = ri.id_ingredient
+                        WHERE ri.id_recette = %(id_recette)s
+                        """,
+                        {
+                            "id_recette": recette.id_recette,
+                        }
+                    )
+                    ingredients = cursor.fetchall()
+
+                    # Ajout des ingrédients à la liste de courses
+                    for ingredient in ingredients:
+                        id_ingredient = ingredient["id_ingredient"]
+                        # Vérification si l'ingrédient est déjà dans la liste de courses
+                        cursor.execute(
+                            "SELECT 1 FROM liste_course WHERE id_utilisateur = %(id_utilisateur)s "
+                            "AND id_ingredient = %(id_ingredient)s",
+                            {
+                                "id_utilisateur": utilisateur.id_utilisateur,
+                                "id_ingredient": id_ingredient,
+                            }
+                        )
+                        if not cursor.fetchone():
+                            cursor.execute(
+                                "INSERT INTO liste_course(id_utilisateur, id_ingredient) "
+                                "VALUES (%(id_utilisateur)s, %(id_ingredient)s)",
+                                {
+                                    "id_utilisateur": utilisateur.id_utilisateur,
+                                    "id_ingredient": id_ingredient,
+                                }
+                            )
+
+            return True  # Tous les ingrédients ont été ajoutés avec succès
+
+        except Exception as e:
+            logging.info(e)
+            return False  # En cas d'erreur
