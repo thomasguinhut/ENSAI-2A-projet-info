b710ef74958f66144684e4e0d1056d40785b0cd5
LIZE Benjamin <id2399@DOMENSAI.ECOLE>
2024-11-12 17:45:46 +0100

    mardi soir

--- src/service/utilisateur_service.py
+++ src/service/utilisateur_service.py
@@ -90,5 +90,9 @@ class UtilisateurService:
     def id_utilisateur_deja_utilise(self, id_utilisateur) -> bool:
         """Vérifie si l'id est déjà utilisé
         Retourne True si l'id existe déjà en BDD"""
-        utilisateurs = UtilisateurDao().lister_tous()
-        return id_utilisateur in [j.id_utilisateur for j in utilisateurs]
+        #print("1")
+        #utilisateurs = UtilisateurDao().lister_tous()
+        #print("2")
+        #print(utilisateurs)
+        return UtilisateurService().trouver_par_id(id_utilisateur) is not None
+        #return id_utilisateur in [j.id_utilisateur for j in utilisateurs]
