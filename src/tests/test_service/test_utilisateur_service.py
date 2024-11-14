24f52bcd1309bfc09e5c1e4e06fe3778e724207c
Toussaint Boco <toussaint.boco@eleve.ensai.fr>
2024-11-14 16:18:10 +0100

    Tests utilisateur service : OK - Toussaint

--- src/tests/test_service/test_utilisateur_service.py
+++ src/tests/test_service/test_utilisateur_service.py
@@ -14,33 +14,33 @@ liste_utilisateurs = [
 ]
 
 
-# def test_creer_ok():
-#     """ Création de l'Utilisateur réussie"""
+def test_creer_ok():
+    """ Création de l'Utilisateur réussie"""
 
-#     # GIVEN
-#     id_utilisateur, mdp = "Tom", "1235"
-#     UtilisateurDao().creer = MagicMock(return_value=True)
+    # GIVEN
+    id_utilisateur, mdp = "Tom", "1235"
+    UtilisateurDao().creer = MagicMock(return_value=True)
 
-#     # WHEN
-#     utilisateur = UtilisateurService().creer(id_utilisateur, mdp)
+    # WHEN
+    utilisateur = UtilisateurService().creer(id_utilisateur, mdp)
 
-#     # THEN
-#     assert utilisateur is not None
+    # THEN
+    assert utilisateur is not None
 
 
-# def test_creer_echec():
-#     """Création de l'Utilisateur échouée
-#     (car la méthode UtilisateurDao().creer retourne False)"""
+def test_creer_echec():
+    """Création de l'Utilisateur échouée
+    (car la méthode UtilisateurDao().creer retourne False)"""
 
-#     # GIVEN
-#     id_utilisateur, mdp = "Ben", "1234"
-#     UtilisateurDao().creer = MagicMock(return_value=False)
+    # GIVEN
+    id_utilisateur, mdp = "Ben", "1234"
+    UtilisateurDao().creer = MagicMock(return_value=False)
 
-#     # WHEN
-#     utilisateur = UtilisateurService().creer(id_utilisateur, mdp)
+    # WHEN
+    utilisateur = UtilisateurService().creer(id_utilisateur, mdp)
 
-#     # THEN
-#     assert utilisateur is None
+    # THEN
+    assert utilisateur is None
 
 
 def test_trouver_par_id():
@@ -48,15 +48,13 @@ def test_trouver_par_id():
 
     # GIVEN
     id_utilisateur = "thomas"
-    mdp = "12345"
-    utilisateur_attendu = Utilisateur(id_utilisateur="thomas", mdp="1234")
-    UtilisateurDao().trouver_par_id = MagicMock(return_value=utilisateur_attendu)
+    # UtilisateurDao().trouver_par_id = MagicMock(return_value=utilisateur_attendu)
 
     # WHEN
     utilisateur_trouve = UtilisateurService().trouver_par_id(id_utilisateur)
 
     # THEN
-    assert utilisateur_trouve == utilisateur_attendu
+    assert utilisateur_trouve.id_utilisateur == id_utilisateur
 
 
 def test_existence_id():
@@ -64,13 +62,12 @@ def test_existence_id():
 
     # GIVEN
     id_utilisateur = "thomas"
-    utilisateur_attendu = Utilisateur(id_utilisateur="thomas", mdp="12345")
-    print(utilisateur_attendu)
+   
     # WHEN
-    utilisateur_trouve = UtilisateurService().trouver_par_id(id_utilisateur)
-    print(utilisateur_trouve)
+    utilisateur_trouve = UtilisateurService().existence_id(id_utilisateur)
+   
     # THEN
-    assert utilisateur_trouve == utilisateur_attendu
+    assert utilisateur_trouve
 
 
 if __name__ == "__main__":
