abfd566b22db0cbdfa50fe06db3d624722e00e1c
LIZE Benjamin <id2399@DOMENSAI.ECOLE>
2024-11-14 16:19:39 +0100

    milieu aprem

--- src/service/ingredient_recette_service.py
+++ src/service/ingredient_recette_service.py
@@ -12,7 +12,6 @@ from dao.ingredient_recette_dao import IngredientRecetteDao
 
 
 class IngredientRecetteService:
-
     """
 
     Création de classe RecetteService.
@@ -48,16 +47,13 @@ class IngredientRecetteService:
 
         liste_ingredients = []
         for nom_ingredient in recette["ingredients_recette"]:
-            id_ingredient = IngredientDao.get_id_ingredient_by_name(
-                nom_ingredient)
+            id_ingredient = IngredientDao.get_id_ingredient_by_name(nom_ingredient)
             if id_ingredient:
                 ingredient = Ingredient(id_ingredient, nom_ingredient)
                 liste_ingredients.append(ingredient)
 
-        id_categorie = CategorieDao.get_id_categorie_by_name(
-            recette["categorie_recette"])
-        id_origine = OrigineDao().get_id_origine_by_name(
-            recette["origine_recette"])
+        id_categorie = CategorieDao.get_id_categorie_by_name(recette["categorie_recette"])
+        id_origine = OrigineDao().get_id_origine_by_name(recette["origine_recette"])
         if id_categorie and id_origine:
             categorie = Categorie(id_categorie, recette["categorie_recette"])
             origine = Origine(id_origine, recette["origine_recette"])
@@ -108,7 +104,26 @@ class IngredientRecetteService:
         liste_ingredient = []
         if res:
             for row in res:
-                ingredient = Ingredient(row["id_ingredient"],
-                                        row["nom_ingredient"])
+                ingredient = Ingredient(row["id_ingredient"], row["nom_ingredient"])
                 liste_ingredient.append(ingredient)
         return liste_ingredient
+
+    @log
+    def liste_str_ingredient_pour_une_recette(self, recette) -> str:
+        """renvoie une chaîne de caractère contenant la liste des ingrédients d'une recette
+
+        Parameters
+        ----------
+        recette : Recette
+
+        Returns
+        -------
+        res : str
+            chaîne de caractères énumérant les différents ingrédients
+        """
+        liste = IngredientRecetteService().lister_ingredients_by_recette(recette.id_recette)
+        res = " a"
+        print(liste)
+        for ingredient in liste:
+            res = res + ingredient.nom_ingredient
+        return res
