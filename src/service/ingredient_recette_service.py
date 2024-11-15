a2c3b59575ec8ebde7ef507c61ee5d763b53db79
RAJAONA Fanilosoa <id2476@DOMENSAI.ECOLE>
2024-11-15 14:35:57 +0100

    test ingredient recette service fini

--- src/service/ingredient_recette_service.py
+++ src/service/ingredient_recette_service.py
@@ -9,10 +9,12 @@ from client.categorie_client import CategorieClient
 from client.origine_client import OrigineClient
 
 from dao.ingredient_recette_dao import IngredientRecetteDao
+from dao.ingredient_dao import IngredientDao
+from dao.categorie_dao import CategorieDao
+from dao.origine_dao import OrigineDao
 
 
 class IngredientRecetteService:
-
     """
 
     Création de classe RecetteService.
@@ -48,16 +50,13 @@ class IngredientRecetteService:
 
         liste_ingredients = []
         for nom_ingredient in recette["ingredients_recette"]:
-            id_ingredient = IngredientDao.get_id_ingredient_by_name(
-                nom_ingredient)
+            id_ingredient = IngredientDao().get_id_ingredient_by_name(nom_ingredient)
             if id_ingredient:
                 ingredient = Ingredient(id_ingredient, nom_ingredient)
                 liste_ingredients.append(ingredient)
 
-        id_categorie = CategorieDao.get_id_categorie_by_name(
-            recette["categorie_recette"])
-        id_origine = OrigineDao().get_id_origine_by_name(
-            recette["origine_recette"])
+        id_categorie = CategorieDao().get_id_categorie_by_name(recette["categorie_recette"])
+        id_origine = OrigineDao().get_id_origine_by_name(recette["origine_recette"])
         if id_categorie and id_origine:
             categorie = Categorie(id_categorie, recette["categorie_recette"])
             origine = Origine(id_origine, recette["origine_recette"])
@@ -75,21 +74,33 @@ class IngredientRecetteService:
             else:
                 return None
 
-    @log
-    def lister_recettes_par_ingredient(self, ingredient) -> list[Recette]:
-        res = IngredientRecetteDao().lister_recettes_par_ingredient(self, ingredient)
-        liste_recettes = []
-        if res:
-            for row in res:
-                recette = Recette(
-                    id_recette=row["id_recette"],
-                    nom_recette=row["nom_recette"],
-                    instructions_recette=row["instructions_recettes"],
-                    id_origine=row["id_origine"],
-                    id_categorie=row["id_categorie"],
-                )
-                liste_recettes.append(recette)
-        return liste_recettes
+    # @log
+    # def lister_recettes_par_ingredient(self, ingredient) -> list[Recette]:
+    #     res = IngredientRecetteDao().lister_recettes_par_ingredient(ingredient)
+    #     print(res)
+    #     liste_recettes = []
+    #     if res:
+    #         for row in res:
+    #             id_origine = row["id_origine"]
+    #             origine = Origine(id_origine, OrigineDao().get_nom_origine_by_id(id_origine))
+    #             id_categorie = row["id_categorie"]
+    #             categorie = Categorie(
+    #                 id_categorie, CategorieDao().get_nom_categorie_by_id(id_categorie)
+    #             )
+    #             res_ingredients = IngredientRecetteDao().lister_ingredients_by_recette(row["id_recette"])
+    #             liste_ingredients_recette = []
+    #             for ingredient in res_ingredients.keys():
+    #                 ingredient = Ingredient(ingredient, res_ingredients[ingredient])
+    #             recette = Recette(
+    #                 id_recette=row["id_recette"],
+    #                 nom_recette=row["nom_recette"],
+    #                 instructions_recette=row["instructions_recette"],
+    #                 origine_recette=origine,
+    #                 categorie_recette=categorie,
+    #                 ingredients_recette=
+    #             )
+    #             liste_recettes.append(recette)
+    #     return liste_recettes
 
     @log
     def lister_ingredients_by_recette(self, id_recette) -> list[dict]:
@@ -108,7 +119,6 @@ class IngredientRecetteService:
         liste_ingredient = []
         if res:
             for row in res:
-                ingredient = Ingredient(row["id_ingredient"],
-                                        row["nom_ingredient"])
+                ingredient = Ingredient(row["id_ingredient"], row["nom_ingredient"])
                 liste_ingredient.append(ingredient)
         return liste_ingredient
