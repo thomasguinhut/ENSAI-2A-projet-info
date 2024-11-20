b1a872b58c561b067dd469af70c857ba0c871c03
Akmal Ayari <akmal.ayari@eleve.ensai.fr>
2024-11-20 17:26:15 +0100

    view

--- src/view/filtres/type_filtre_vue.py
+++ src/view/filtres/type_filtre_vue.py
@@ -16,7 +16,11 @@ class TypeFiltreVue(VueAbstraite):
         """Affiche le menu pour choisir le type de filtre."""
         HistoriqueVues().ajouter_vue(self)
         choix = inquirer.select(
-            message="Choisissez votre type de filtre:",
+            message="Choisissez votre type de filtre:\n\nAttention: les filtres sont inclusifs dans"
+            " un même type de filtre et exclusifs entre deux types de filtres différents. Exemple: "
+            "les filtres Challots et Butter affichent les recettes contenant du beurre ou des "
+            "échallotes tandis que les filtres Butter et Mexican affichent les recettes qui "
+            "contiennent du beurre et qui sont mexicaines.\n",
             choices=["Ingrédient", "Catégorie", "Origine"],
         ).execute()
 
